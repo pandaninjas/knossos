6b9bda5d10866496b8273ab19b4243bb7099edc0
Emma Triphora <emma@modrinth.com>
2023-02-12 00:49:12 -0500

    The downsides of not being able to set up a local testing env, in a commit

--- pages/index.vue
+++ pages/index.vue
@@ -550,9 +550,7 @@ const [
   useAsyncData('updatedProjects', () => useBaseFetch(`search?limit=3&query=&index=updated`), {
     transform: (result) => result.hits,
   }),
-  useAsyncData('statistics', () => useBaseFetch(`statistics`), {
-    transform: (result) => result.hits,
-  }),
+  useAsyncData('statistics', () => useBaseFetch(`statistics`)),
 ])
 </script>
 
